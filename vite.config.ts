516700ed44786e8338553e5747073bb2b9902bce
Yogi Priyan Perdana <yogikid@proton.me>
2026-07-25 09:11:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: {
-    preset: "node-server",
-  },
   tanstackStart: {
     server: { entry: "server" },
   },
